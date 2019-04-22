ba6c2299ae4ff8a0cc2dd4fec32a43b49eacd5e6
Vetrivel K S <vetrivel.ks@skava.com>
2019-04-22 23:18:11 +0530

    boiler plate init with basic routing

---
+++ src/views/PageTwo/PageTwo.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+// import "./App.css";
+
+class PageTwo extends Component {
+  render() {
+    return (
+      <div className="page-two">
+        <h1> Page Two </h1>
+      </div>
+    );
+  }
+}
+
+export { PageTwo }
+export default PageTwo
